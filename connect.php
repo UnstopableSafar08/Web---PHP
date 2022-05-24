4e065ea054dd9f000d4897d450f00a827db19567
sagar <sagarmalla08@gmail.com>
2022-05-24 17:26:41 +0545

    db conn tested

--- connect.php
+++ connect.php
@@ -1,9 +1,4 @@
 <?php session_start();
 
-<<<<<<< HEAD
-$con=  mysqli_connect("localhost","root","","test")
-?>
-=======
 $con=  mysqli_connect("13.235.243.98","sagar","sagar@123","test")
 ?>
->>>>>>> c9c3a229068021014b7085ac070ccb76735a2eab
